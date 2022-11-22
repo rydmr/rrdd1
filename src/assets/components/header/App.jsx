4321cf6494bbba202cef739559c1333e804f7301
rydmr <115474621+rydmr@users.noreply.github.com>
2022-11-22 15:44:52 +0400

    Add files via upload

---
+++ src/assets/components/header/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+   return (
+    <div class="hello">
+        <h3>Demirova Roya</h3>
+        <p>Now l can create component</p>
+    
+    </div>
+   )
+
+}
+
+export default App
